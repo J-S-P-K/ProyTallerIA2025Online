eb7febc7726bb4e53bc0987e0c246a06b2eb71f9
J-S-P-K <juasantipeko@gmail.com>
2025-11-06 20:25:55 -0300

    Update app.py
    DEBERIA FUNCIONAR

--- app/app.py
+++ app/app.py
@@ -240,9 +240,4 @@ def connect():
 
 @socketio.on('disconnect')
 def test_disconnect():
-
     print('Client disconnected', request.sid)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
